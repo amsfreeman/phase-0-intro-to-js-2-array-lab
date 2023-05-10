e8c2c9b7363f8086f4a21de5b02c3db93626ba45
Amelia Freeman <ameliakfreeman@gmail.com>
2023-05-10 12:26:09 -0500

    array tests passed

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
-// Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name) {
+    cats.push(name);
+};
+
+function destructivelyPrependCat(name) {
+    cats.unshift(name);
+};
+
+function destructivelyRemoveLastCat(name) {
+    cats.pop(name);
+};
+
+function destructivelyRemoveFirstCat(name) {
+    cats.shift(name);
+};
+
+const copyOfCats = [...cats];
+function appendCat(name) {
+   const allCatsAppend = [...cats, name];
+   return allCatsAppend;
+};
+
+function prependCat(name) {
+    const allCatsPrepend = [name, ...cats];
+    return allCatsPrepend;
+};
+
+function removeLastCat() {
+   return cats.slice(0, cats.length -1); 
+}
+
+function removeFirstCat() {
+    return cats.slice(1);
+}
